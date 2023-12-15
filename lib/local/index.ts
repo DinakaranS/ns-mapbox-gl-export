28aeca890b11953d005f7f39ecdd75cda0613c6a
dinakaran <dinakaran@nobel-systems.com>
2023-12-15 09:27:29 +0530

    initial commit

---
+++ lib/local/index.ts
@@ -0,0 +1,24 @@
+import english from './en';
+import french from './fr';
+import finnish from './fi';
+import german from './de';
+import swedish from './sv';
+import vietnam from './vi';
+
+type Translation = {
+  PageSize: string;
+  PageOrientation: string;
+  Format: string;
+  DPI: string;
+  Generate: string;
+}
+
+export {
+  english,
+  french,
+  finnish,
+  german,
+  swedish,
+  vietnam,
+  Translation,
+};
